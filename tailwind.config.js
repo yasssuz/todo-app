b27a744b1aade276a7897ca2f3d11e3519a5eb04
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-09 17:48:24 -0300

    added colors

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: "class",
+  theme: {
+    colors: {
+      lightTheme: {
+        veryLightGray: "hsl(0, 0%, 98%)",
+        veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+        lightGrayishBlue: "hsl(233, 11%, 84%)",
+        darkGrayishBlue: "hsl(236, 9%, 61%)",
+        veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+      },
+      darkTheme: {
+        veryDarkBlue: "hsl(235, 21%, 11%)",
+        veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+        lightGrayishBlue: "hsl(234, 39%, 85%)",
+        lightGrayishBlueHovered: "hsl(236, 33%, 92%)",
+        darkGrayishBlue: "hsl(234, 11%, 52%)",
+        veryDarkGrayishBlue: "hsl(233, 14%, 35%)",
+        darkestGrayishBlue: "hsl(237, 14%, 26%)",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
