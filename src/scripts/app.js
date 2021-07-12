1bcdd89e499dbd6ac1316ed80ef2214423a4dbba
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-12 16:26:02 -0300

    added observer and addTodo feature

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,32 @@
 import { getInitialTheme, toggleTheme } from "./theme";
-import { markTodo, removeTodo } from "./todo";
+import { addTodo, markTodo, removeTodo } from "./todo";
 import { getAllQueries, getQuery } from "./utils";
 
+const form = document.forms.todoCreation;
+
+// change theme feature
 getQuery("#toggle-theme-btn").addEventListener("click", toggleTheme);
-getAllQueries(".checkbox-completed").forEach(element =>
-  element.addEventListener("click", markTodo)
-);
-getAllQueries(".remove-todo").forEach(element =>
-  element.addEventListener("click", removeTodo)
-);
+
+// saved/initial theme feature
 window.addEventListener("load", getInitialTheme);
+
+// observe changes on the todos list
+const observer = new MutationObserver(() => {
+  // mark todo as done feature
+  getAllQueries(".checkbox-completed").forEach(element =>
+    element.addEventListener("click", markTodo)
+  );
+
+  // remove todo feature
+  getAllQueries(".remove-todo").forEach(element =>
+    element.addEventListener("click", removeTodo)
+  );
+});
+
+// add todo feature
+form.addEventListener("submit", e => addTodo(e, form));
+
+// set observer config
+observer.observe(getQuery(".todos-list"), {
+  childList: true,
+});
